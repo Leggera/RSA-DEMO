985ff319c0a146551dc06b1b8ebcdeb467bd4cdc
Leggera <lana.krlv1995@gmail.com>
2018-12-18 04:50:28 +0300

    forms saved

--- demo/views.py
+++ demo/views.py
@@ -3,6 +3,8 @@ from .forms import PostForm
 
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import redirect, get_object_or_404
+from .models import Post
 
 def hello(request):
 	return HttpResponse('hello')
@@ -25,11 +27,28 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'demo/post_edit.html', {'form': form})
+    
+    
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            #post.author = request.user
+            #post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'demo/post_edit.html', {'form': form})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'demo/post_detail.html', {'post': post})
 
--- website/urls.py
+++ website/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     #path('', views.text_form, name='text_form'),
     path('', views.post_list, name='post_list'),
     path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
